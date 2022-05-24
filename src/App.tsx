7907fe5fef190a3fa402cb66430c83cb186a9bca
Antonini333 <pabloantoninivazquez@gmail.com>
2022-05-24 19:20:58 +0200

    Testing useInterval from 'usehooks-ts'

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import logo from './logo.svg';
+import { useInterval } from "usehooks-ts"
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState<number>(0)
+  // Dynamic delay
+  const [delay, setDelay] = useState<number>(1000)
+  // ON/OFF
+  const [isPlaying, setPlaying] = useState<boolean>(false)
+
+  useInterval(
+    () => {
+      // Your custom logic here
+      setCount(count + 1)
+    },
+    // Delay in milliseconds or null to stop it
+    isPlaying ? delay : null,
+  )
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setDelay(Number(event.target.value))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setPlaying(!isPlaying)}>
+        {isPlaying ? 'pause' : 'play'}
+      </button>
+      <p>
+        <label htmlFor="delay">Delay: </label>
+        <input
+          type="number"
+          name="delay"
+          onChange={handleChange}
+          value={delay}
+        />
+      </p>
+    </>
+  )
 }
 
 export default App;
