cdfc2cf8806cb242fcee6b5f08a4866a97fe354d
Antonini333 <pabloantoninivazquez@gmail.com>
2022-05-24 20:07:00 +0200

    Started necessary state variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,18 @@ import './App.css';
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
-
+  const [ snake, setSnake ] = useState<any>()
+	const [ apple, setApple ] = useState<any>()
+	const [ direction, setDirection ] = useState<any>()
+	const [ gameOver, setGameOver ] = useState<boolean>(false)
+	const [ score, setScore ] = useState<number>(0)
   return (
     <>
       <div className="main-container">
+			
 			<canvas className="play-area" ref={canvasRef}  />
 			
-			<button onClick={() => {}} className="play-button">
+			<button onClick={() => {}} className="playButton">
 				Play
 			</button>
 			<div className="scoreBox">
