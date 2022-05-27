872c6c5f6bafb570475385b2149a283811e9ee75
Antonini333 <pabloantoninivazquez@gmail.com>
2022-05-27 13:51:33 +0200

    Created constants to manage initial states

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
-import React, { ChangeEvent, useRef, useState } from 'react';
-
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import AppleLogo from "./assets/appleLogo.png"
 import { useInterval } from "usehooks-ts"
 import './App.css';
 
+const initialSnake = [[4, 10], [4, 10]];
+const scale = 50
+const canvasX = 1000
+const canvasY = 1000
+
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null)
-  const [ snake, setSnake ] = useState<[number, number][]>([ [ 4, 10 ], [ 4, 10 ] ])
-	const [ apple, setApple ] = useState<number[]>([ 14, 10 ])
-	const [ direction, setDirection ] = useState<number[]>([ 0, -1 ])
-	const [ gameOver, setGameOver ] = useState<boolean>(false)
-	const [ score, setScore ] = useState<number>(0)
-  
-  return (
-    <>
-      <div className="main-container">
-			<canvas className="play-area" ref={canvasRef}  />
-			<button onClick={() => {}} className="playButton">
-				Play
+	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const [snake, setSnake] = useState(initialSnake)
+	const [apple, setApple] = useState<number[]>([14, 10])
+	const [direction, setDirection] = useState<number[]>([0, -1])
+	const [gameOver, setGameOver] = useState<boolean>(false)
+	const [score, setScore] = useState<number>(0)
+
+
+
+	return (
+		<>
+			<div onKeyDown={() => { }}>
+				<img id="fruit" src={AppleLogo} alt="fruit" width="30" />
+				<canvas className="playArea" ref={canvasRef} width={`${canvasX}px`} height={`${canvasY}px`} />
+				{gameOver && <div className="gameOver">Game Over</div>}
+				<button className="playButton">
+					Play
 			</button>
-			<div className="scoreBox">
-				<h2>Score: </h2>
-				<h2>High Score: </h2>
+				<div className="scoreBox">
+					<h2>Score: {score}</h2>
+					<h2>High Score: </h2>
+				</div>
 			</div>
-		</div>
-    </>
-  )
+		</>
+	)
 }
 
 export default App;
